6577b9e5fbbdadf7b4efe549de0192b00d2a82b7
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-09-03 18:23:17 +0530

    n=int(input()) a=list(map(int,input().split())) s1=0 s2=0 for i in range(n):     if i%2==0:         s1+=a[i]     else:         s2+=a[i] d=s1-s2 if d==0:     print('YES') else:     print('NO')

---
+++ Sum_of_odd_and_even_digits.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+s1=0
+s2=0
+for i in range(n):
+    if i%2==0:
+        s1+=a[i]
+    else:
+        s2+=a[i]
+d=s1-s2
+if d==0:
+    print('YES')
+else:
+    print('NO')
