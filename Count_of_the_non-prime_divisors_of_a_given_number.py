62b32a24a4c3351d6f42362b550223ed52d276ce
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-09-07 19:28:36 +0530

    n=int(input()) c=[] for i in range(1,n+1):     if n%i==0:         c.append(i) x=min(c) y=max(c) d=[] for i in range(x,y):     if i>1:         for j in range(2,i):             if i%j==0:                 break         else:             d.append(i) e=[] for i in range(len(c)):     if c[i] in d:         pass     else:         e.append(c[i]) print(len(e))

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,15 +1,22 @@
 n=int(input())
 c=[]
+for i in range(1,n+1):
+    if n%i==0:
+        c.append(i)
+x=min(c)
+y=max(c)
 d=[]
-for i in range(n+1):
+for i in range(x,y):
     if i>1:
         for j in range(2,i):
             if i%j==0:
                 break
         else:
-            c.append(i)
-for i in range(1,n+1):
-    if n%i==0:
-        if i not in c:
             d.append(i)
-print(len(d))
+e=[]
+for i in range(len(c)):
+    if c[i] in d:
+        pass
+    else:
+        e.append(c[i])
+print(len(e))
