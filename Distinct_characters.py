97940dd5526454b4adfb82a1bee402581656deff
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-09-08 20:15:45 +0530

    n=input() n=n.lower() c=[] d=[] e=[] for i in range(len(str(n))):     if n[i]!=' ':         if n[i] not in c:             c.append(n[i])         else:             d.append(n[i]) d=list(set(d)) for i in range(len(c)):     if c[i] not in d:         e.append(c[i]) e.sort() for i in range(len(e)):     print(e[i],end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,10 +1,18 @@
 n=input()
 n=n.lower()
 c=[]
+d=[]
+e=[]
 for i in range(len(str(n))):
     if n[i]!=' ':
         if n[i] not in c:
             c.append(n[i])
-c.sort()
+        else:
+            d.append(n[i])
+d=list(set(d))
 for i in range(len(c)):
-    print(c[i],end='')
+    if c[i] not in d:
+        e.append(c[i])
+e.sort()
+for i in range(len(e)):
+    print(e[i],end='')
