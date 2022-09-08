817267665a804d7ba59c8a3bbb82fa08f8faf86f
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-09-08 20:03:38 +0530

    n=input() n=n.lower() c=[] d=[] e=[] for i in range(len(str(n))):     if n[i]!=' ':         if n[i] not in c:             c.append(n[i])         else:             d.append(n[i]) d=list(set(d)) for i in range(len(c)):     if c[i] not in d:         e.append(c[i]) print(len(e))

---
+++ count_unique_characters.py
@@ -0,0 +1,16 @@
+n=input()
+n=n.lower()
+c=[]
+d=[]
+e=[]
+for i in range(len(str(n))):
+    if n[i]!=' ':
+        if n[i] not in c:
+            c.append(n[i])
+        else:
+            d.append(n[i])
+d=list(set(d))
+for i in range(len(c)):
+    if c[i] not in d:
+        e.append(c[i])
+print(len(e))
