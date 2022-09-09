7ab29fe60b9ed5371ec9feb14fe0ca73c2cfa594
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-09-09 14:54:05 +0530

    s1=input() s1=s1.lower() s2=input() s2=s2.lower() c=[] d=[] e=[] for i in range(len(str(s1))):     if s1[i]!=' ':         c.append(s1[i]) for i in range(len(str(s2))):     if s2[i]!=' ':         d.append(s2[i]) for i in range(len(c)):     for j in range(len(d)):         if c[i]==d[j]:             e.append(c[i]) e=list(set(e)) print(len(e))

---
+++ common_characters_-II.py
@@ -0,0 +1,19 @@
+s1=input()
+s1=s1.lower()
+s2=input()
+s2=s2.lower()
+c=[]
+d=[]
+e=[]
+for i in range(len(str(s1))):
+    if s1[i]!=' ':
+        c.append(s1[i])
+for i in range(len(str(s2))):
+    if s2[i]!=' ':
+        d.append(s2[i])
+for i in range(len(c)):
+    for j in range(len(d)):
+        if c[i]==d[j]:
+            e.append(c[i])
+e=list(set(e))
+print(len(e))
