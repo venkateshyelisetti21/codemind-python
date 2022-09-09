cec669fa166cd7db8ff7b3f6e1e30daa0141b593
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-09-09 19:25:55 +0530

    s1=input() s1=s1.lower() s2=input() s2=s2.lower() c=[] d=[] e=[] for i in range(len(str(s1))):     if s1[i]!=' ':         c.append(s1[i]) for i in range(len(str(s2))):     if s2[i]!=' ':         d.append(s2[i]) for i in range(len(c)):     if c[i]not in d:         e.append(c[i]) for i in range(len(d)):     if d[i]not in c:         e.append(d[i]) e=list(set(e)) e.sort() if len(e):     for i in range(len(e)):         print(e[i],end='') else:     print(-1)

---
+++ non_common_characters.py
@@ -0,0 +1,26 @@
+s1=input()
+s1=s1.lower()
+s2=input()
+s2=s2.lower()
+c=[]
+d=[]
+e=[]
+for i in range(len(str(s1))):
+    if s1[i]!=' ':
+        c.append(s1[i])
+for i in range(len(str(s2))):
+    if s2[i]!=' ':
+        d.append(s2[i])
+for i in range(len(c)):
+    if c[i]not in d:
+        e.append(c[i])
+for i in range(len(d)):
+    if d[i]not in c:
+        e.append(d[i])
+e=list(set(e))
+e.sort()
+if len(e):
+    for i in range(len(e)):
+        print(e[i],end='')
+else:
+    print(-1)
