280f7d0ff563236b1431e0378a13bd42a590ad85
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-09-17 18:49:29 +0530

    a,b=map(int,input().split()) if a>b:tmp=a else:tmp=b lcm=0 while True:     if tmp%a==0 and  tmp%b==0:         lcm=tmp         break     tmp+=1 print(lcm)

--- LCM.py
+++ LCM.py
@@ -1,11 +1,10 @@
 a,b=map(int,input().split())
-lcm=0
 if a>b:tmp=a
 else:tmp=b
+lcm=0
 while True:
-    if tmp%a==0 and tmp%b==0:
+    if tmp%a==0 and  tmp%b==0:
         lcm=tmp
         break
     tmp+=1
 print(lcm)
-    
