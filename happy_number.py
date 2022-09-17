66b72ba036f212ade390d2dd1654a3d5095e39de
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-09-17 15:04:06 +0530

    def fun(n):     s=0     while n:         r=n%10         s+=(r**2)         n//=10     n=s     if s>=9:         return fun(s)     else:         return s
    n=int(input())
    tmp=fun(n)
    if tmp==1 or tmp==7:
        print(True)
    else:
        print(False)

---
+++ happy_number.py
@@ -0,0 +1,18 @@
+def fun(n):
+    s=0
+    while n:
+        r=n%10
+        s+=(r**2)
+        n//=10
+    n=s
+    if s>=9:
+        return fun(s)
+    else:
+        return s
+
+n=int(input())
+tmp=fun(n)
+if tmp==1 or tmp==7:
+    print(True)
+else:
+    print(False)
