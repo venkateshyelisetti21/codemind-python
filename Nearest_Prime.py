a1f8754e5c1f712451d10658f92a329641974ce6
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-10-14 15:44:39 +0530

    num=int(input()) for i in range(num):     n=int(input())     tmp=n     a=0     while True:         for i in range(2,tmp//2 +1):             if tmp%i==0:                 break         else:             a=tmp             break         tmp+=1     tmp1=n     b=0     while True:         for i in range(2,tmp1//2 +1):             if tmp1%i==0:                 break         else:             b=tmp1             break         tmp1-=1     x=abs(n-a)     y=abs(n-b)     if x<y:         print(a)     else:print(b)

---
+++ Nearest_Prime.py
@@ -0,0 +1,28 @@
+num=int(input())
+for i in range(num):
+    n=int(input())
+    tmp=n
+    a=0
+    while True:
+        for i in range(2,tmp//2 +1):
+            if tmp%i==0:
+                break
+        else:
+            a=tmp
+            break
+        tmp+=1
+    tmp1=n
+    b=0
+    while True:
+        for i in range(2,tmp1//2 +1):
+            if tmp1%i==0:
+                break
+        else:
+            b=tmp1
+            break
+        tmp1-=1
+    x=abs(n-a)
+    y=abs(n-b)
+    if x<y:
+        print(a)
+    else:print(b)
