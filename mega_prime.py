a974f9dba6107add748e2ef103aaafc2de6201ab
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-10-14 14:56:58 +0530

    n=int(input()) l=len(str(n)) a=[2,3,5,7] b=0 ctr=0 for i in range(2,int(n**.5) +1):     if n%i==0:         break else:    b=True if b==True:     while n:         r=n%10         if r in a:             ctr+=1         n//=10     if l==ctr:         print('Mega Prime')     else:print('Not Mega Prime') else:print('Not Mega Prime')

--- mega_prime.py
+++ mega_prime.py
@@ -1,19 +1,20 @@
 n=int(input())
-a=0
+l=len(str(n))
+a=[2,3,5,7]
 b=0
-c=[2,3,5,7]
-for i in range(2,n):
+ctr=0
+for i in range(2,int(n**.5) +1):
     if n%i==0:
         break
 else:
-    a=True
-while n:
- r=n%10
- if r in c:
-     b=True
- else:b=False
- n//=10
-if a==True and b==True:
-    print('Mega Prime')
-else:
-    print('Not Mega Prime')
+   b=True
+if b==True:
+    while n:
+        r=n%10
+        if r in a:
+            ctr+=1
+        n//=10
+    if l==ctr:
+        print('Mega Prime')
+    else:print('Not Mega Prime')
+else:print('Not Mega Prime')
