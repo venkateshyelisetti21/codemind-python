3b3d8d78882d9e7a9f77850a28e9702426c5b3e2
venkateshyelisetti21 <109537237+venkateshyelisetti21@users.noreply.github.com>
2022-11-06 08:55:01 +0530

    n=int(input()) m=list(map(int,input().split())) a=[] b=[] c=[] d=0 for i in range(len(m)):     if m[i]%2==0:         a.append(m[i])     else:         b.append(m[i]) e=min(len(a),len(b)) if len(a)>len(b):     f=a else:     f=b while d<len(f):     if d<e:
            c.append(b[d])
            c.append(a[d])
        else:
            c.append(f[d])
        d+=1
    if n%2:
        c.append(0)
    print(*c)

---
+++ odd_even_pairs.py
@@ -0,0 +1,27 @@
+n=int(input())
+m=list(map(int,input().split()))
+a=[]
+b=[]
+c=[]
+d=0
+for i in range(len(m)):
+    if m[i]%2==0:
+        a.append(m[i])
+    else:
+        b.append(m[i])
+e=min(len(a),len(b))
+if len(a)>len(b):
+    f=a
+else:
+    f=b
+while d<len(f):
+    if d<e:
+        
+        c.append(b[d])
+        c.append(a[d])
+    else:
+        c.append(f[d])
+    d+=1
+if n%2:
+    c.append(0)
+print(*c)
